dae2a743d4f3abc57ecb538e424cc79e16871a01
GuteamG <user.email>
2023-11-02 20:23:07 +0300

    add first content

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 function App() {
-  const [count, setCount] = useState(0)
+  const [campaigns, setCampaigns] = useState([]);
+
+  const fetchCampaigns = async () => {
+    try {
+      const result = await axios.get(
+        "https://www.plugco.in/public/take_home_sample_feed"
+      );
+      setCampaigns(result.data.campaigns);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchCampaigns();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {campaigns.length && (
+        <div className="d-flex flex-column gap-3 p-3">
+          <div className="d-flex gap-2">
+            <div>
+              <img
+                src={campaigns[0].campaign_icon_url}
+                alt="icon"
+                className="rounded"
+                width={56}
+                height={56}
+              />
+            </div>
+            <div className="d-flex flex-column gap-1">
+              <div className="fs-5 fw-bold">{campaigns[0].campaign_name}</div>
+              <div>{campaigns[0].pay_per_install} per install</div>
+            </div>
+          </div>
+          <div className="d-flex overflow-auto p-2">
+            {campaigns[0].medias.map((media) => (
+              <div className="d-flex gap-5 rounded" key={media.cover_photo_url}>
+                <div className="d-flex flex-column gap-1">
+                  <div className="image">
+                    <img
+                      src={media.cover_photo_url}
+                      alt={`Cover for ${campaigns[0].campaign_name}`}
+                      className="img-fluid rounded"
+                      style={{
+                        minWidth: "80px",
+                        maxWidth: "200px",
+                        height: "auto",
+                      }}
+                    />
+                  </div>
+                  <div className="icons d-flex gap-2">
+                    <span>copy link</span>
+                    <span>download</span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
