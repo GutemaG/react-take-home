888935f1848544a3bd054573a44f3431cecf8df0
GuteamG <user.email>
2023-11-02 22:56:17 +0300

    add icon

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -38,19 +38,44 @@ export const Media = ({ media }) => {
             onEnded={handlePauseVideo}
           />
         ) : (
-          <img
-            src={media.cover_photo_url}
-            alt={`alt`}
-            className="img-fluid rounded-2 border m-1"
-            style={{
-              minWidth: "55px",
-              maxWidth: "130px",
-              height: "auto",
-            }}
-            onClick={handlePlayVideo}
-          />
+          <div style={{ position: "relative" }}>
+            <img
+              src={media.cover_photo_url}
+              alt={`alt`}
+              className="img-fluid rounded-2 border m-1"
+              style={{
+                minWidth: "55px",
+                maxWidth: "130px",
+                height: "auto",
+                background: "rgba(0, 0, 0, 0.5)",
+              }}
+            />
+            <span
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                cursor: "pointer",
+              }}
+              onClick={handlePlayVideo}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="66"
+                height="56"
+                fill="currentColor"
+                className="bi bi-file-play text-light"
+                viewBox="0 0 16 16"
+              >
+                <path d="M6 10.117V5.883a.5.5 0 0 1 .757-.429l3.528 2.117a.5.5 0 0 1 0 .858l-3.528 2.117a.5.5 0 0 1-.757-.43z" />
+                <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z" />
+              </svg>
+            </span>
+          </div>
         )}
       </div>
+
       <div className="d-flex gap-2">
         <button
           className="btn"
